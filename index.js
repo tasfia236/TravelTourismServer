61ffbb89bf6848a80e83dd700e58f95447b124a7
tasfia236 <tasfiatabassum300@gmail.com>
2024-06-22 22:41:05 +0600

    Finished

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const cors = require("cors");
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 4000;
 
-app.use(cors());
+// middleware
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://tourist-guide-e209e.web.app",
+        "https://tourist-guide-e209e.firebaseapp.com"
+    ],
+    credentials: true,
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxwisw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -25,7 +32,7 @@ let requestedUsers = [];
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect()
 
         const userCollection = client.db("touristGuideDB").collection("users");
         const spotCollection = client.db("touristGuideDB").collection("spots");
@@ -33,6 +40,7 @@ async function run() {
         const bookCollection = client.db("touristGuideDB").collection("books");
         const guideRequestsCollection = client.db("touristGuideDB").collection("requests");
 
+        
         // jwt related api
         app.post('/jwt', async (req, res) => {
             const user = req.body;
@@ -231,7 +239,6 @@ async function run() {
         })
 
 
-
         //Spot related API
         app.get('/spots', async (req, res) => {
             const result = await spotCollection.find().toArray();
@@ -321,7 +328,7 @@ async function run() {
         });
 
 
-
+        
         //Story api
         app.get('/stories', async (req, res) => {
             const result = await storyCollection.find().toArray();
@@ -418,15 +425,25 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/booking/cancel/:id', async (req, res) => {
+            const id = req.params.id;
+            try {
+                const result = await bookCollection.deleteOne({ _id: new ObjectId(id) });
+                res.status(200).json(result);
+            } catch (error) {
+                console.error('Error deleting wishlist item:', error);
+                res.status(500).json({ message: 'Failed to delete wishlist item' });
+            }
+        });
+
+
 
 
         app.get('/', (req, res) => {
-            res.send('Welcome to Our Tourist Guide!');
+            res.send('running');
         })
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -435,6 +452,7 @@ async function run() {
 run().catch(console.dir);
 
 
-app.listen(port, (req, res) => {
-    console.log('listening on port ' + port);
+
+app.listen(port, () => {
+    console.log(`brand server is running on port ${port}`);
 })
